a881fab1dd49e4813d28f17230fef87bfd34948d
Elias Fernandes <eliasfernandescout@gmail.com>
2023-12-22 19:10:44 -0300

    chore: first commit

---
+++ src/main/java/basiccalcback/BasiccalcApplication.java
@@ -0,0 +1,13 @@
+package basiccalcback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasiccalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasiccalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/basiccalcback/BasiccalcApplicationTests.java
@@ -0,0 +1,13 @@
+package basiccalcback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasiccalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
